9fd4fdfcd93e9e24f2aaab782d6699be167bc8d4
Razanalshatti <83327@kfh.com>
2024-04-14 10:53:56 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Drawing;
+
+// task 1 C# primitve types
+/*
+string name = "Razan";
+int age = 23;
+int number = 29;
+double gpa = 3.29;
+bool graduated = true;
+
+Console.WriteLine($" Name: {name}");
+Console.WriteLine($" Age: {age}");
+Console.WriteLine($" Number: {number:00.00}");
+Console.WriteLine($" GPA: {gpa}");
+Console.WriteLine($" is graduated?: {graduated}");
+Console.ReadLine();
+*/
+
+// task 2 C# variables
+
+string studentName = "Razan Alshatti";
+char grade = 'A';
+string favoriteSubject = "PE";
+double height = 3.2;
+double width = 5.5;
+
+Console.WriteLine($"Student name: {studentName}");
+Console.WriteLine($"Student favorite subject: {favoriteSubject}");
+Console.WriteLine($"Student grade: {grade}");
+Console.WriteLine($"Area: {height*width}");
+Console.ReadLine();
+
+
+
+
